258a7a3a52067621cc6558c8942abb14919dee0c
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-06-06 08:17:53 +0530

    Old stream delete

--- jk_dev/__init__.py
+++
@@ -1,9 +0,0 @@
-# Kanged From @JK_DEV
-from aiohttp import web
-from jk_dev.stream_routes import routes
-
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
